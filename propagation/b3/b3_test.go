d525b75a45561b432246971f8601c45b07b0ab94
Chris Reeves <hello@chris.reeves.io>
2019-08-19 17:27:31 +0100

    feat(b3): test cases for span context from message attributes

--- propagation/b3/b3_test.go
+++ propagation/b3/b3_test.go
@@ -151,3 +151,118 @@ func TestSpanContextToMessageAttributes(t *testing.T) {
 		})
 	}
 }
+
+func TestSpanContextFromMessageAttributes(t *testing.T) {
+	type TestCase struct {
+		tName string
+		in    interface{}
+		sc    trace.SpanContext
+		ok    bool
+	}
+	tt := []TestCase{
+		{
+			tName: "sns not sampled",
+			in: map[string]*sns.MessageAttributeValue{
+				TraceIDKey: &sns.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String(ocawstest.DefaultTraceID.String()),
+				},
+				SpanIDKey: &sns.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String(ocawstest.DefaultSpanID.String()),
+				},
+				SpanSampledKey: &sns.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String("0"),
+				},
+			},
+			sc: trace.SpanContext{
+				TraceID:      ocawstest.DefaultTraceID,
+				SpanID:       ocawstest.DefaultSpanID,
+				TraceOptions: trace.TraceOptions(0),
+			},
+			ok: true,
+		},
+		{
+			tName: "sqs not sampled",
+			in: map[string]*sqs.MessageAttributeValue{
+				TraceIDKey: &sqs.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String(ocawstest.DefaultTraceID.String()),
+				},
+				SpanIDKey: &sqs.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String(ocawstest.DefaultSpanID.String()),
+				},
+				SpanSampledKey: &sqs.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String("0"),
+				},
+			},
+			sc: trace.SpanContext{
+				TraceID:      ocawstest.DefaultTraceID,
+				SpanID:       ocawstest.DefaultSpanID,
+				TraceOptions: trace.TraceOptions(0),
+			},
+			ok: true,
+		},
+		{
+			tName: "sns sampled",
+			in: map[string]*sns.MessageAttributeValue{
+				TraceIDKey: &sns.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String(ocawstest.DefaultTraceID.String()),
+				},
+				SpanIDKey: &sns.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String(ocawstest.DefaultSpanID.String()),
+				},
+				SpanSampledKey: &sns.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String("1"),
+				},
+			},
+			sc: trace.SpanContext{
+				TraceID:      ocawstest.DefaultTraceID,
+				SpanID:       ocawstest.DefaultSpanID,
+				TraceOptions: trace.TraceOptions(1),
+			},
+			ok: true,
+		},
+		{
+			tName: "sqs sampled",
+			in: map[string]*sqs.MessageAttributeValue{
+				TraceIDKey: &sqs.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String(ocawstest.DefaultTraceID.String()),
+				},
+				SpanIDKey: &sqs.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String(ocawstest.DefaultSpanID.String()),
+				},
+				SpanSampledKey: &sqs.MessageAttributeValue{
+					DataType:    aws.String("String"),
+					StringValue: aws.String("1"),
+				},
+			},
+			sc: trace.SpanContext{
+				TraceID:      ocawstest.DefaultTraceID,
+				SpanID:       ocawstest.DefaultSpanID,
+				TraceOptions: trace.TraceOptions(1),
+			},
+			ok: true,
+		},
+	}
+	for _, tc := range tt {
+		tc := tc
+		t.Run(tc.tName, func(t *testing.T) {
+			t.Parallel()
+
+			p := New()
+			sc, ok := p.SpanContextFromMessageAttributes(tc.in)
+
+			assert.Equal(t, tc.ok, ok)
+			assert.Equal(t, tc.sc, sc)
+		})
+	}
+}
